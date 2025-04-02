733ee87201e0e2f207f4cbaa7e309e9fd09bd26b
Rishu kumar singh <rishu272018@gmail.com>
2025-04-02 18:47:13 +0530

    first day of cpp

---
+++ 31mar.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    // int x = 10;
+    // cout<<"The value of x"<<x<<endl;
+
+    // x+=10;
+    //  cout<<"The value of x"<<x<<endl;
+
+    //  x-=2;
+    //   cout<<"The value of x"<<x<<endl;
+
+    //  x/=2;
+    //    cout<<"The value of x"<<x<<endl;
+    // x%=2;
+    //   cout<<"The value of x"<<x<<endl;
+    //   x*=2;
+    //     cout<<"The value of x"<<x<<endl;
+    // int a,b,c;
+    // a=5;
+    // b=4;
+    // c=a+b;
+    // cout<<"The addition of a and b = \n"<<c<<endl;
+    // c=a*b;
+    // cout<<"The multipliction of a and b = \n"<<c<<endl;
+    // c=a-b;
+    // cout<<"The sub of a and b = \n"<<c<<endl;
+    // c=a/b;
+    // cout<<"The division of a and b = \n"<<c<<endl;
+    // c=a%b;
+    // cout<<"The remainder of a and b = \n"<<c<<endl;
+
+    // find the Area
+
+    //
+
+    // int a, b, c;
+    // float d ,r12, r22;
+    // cout << "enter value of a,b,c\n";
+    // cin >> a >> b >> c;
+    // d = (b * b) - 4 * a * c;
+    // cout << "The value of d = \n"<< d << endl;
+    // r12 = (-b + sqrt(d)) / (2 * a);
+    // r22 = (-b - sqrt(d)) / (2 * a);
+    // if(d < 0)
+    // {
+    //     cout << "The roots are imaginary\n";
+    //     cout << "The value of r1 = \n"<< r12 << endl;
+    //     cout << "The value of r2 = \n"<< r22 << endl;
+    // }
+    // else if(d == 0)
+    // {
+    //     cout << "The roots are real and equal\n";
+    //     cout << "The value of r1 = \n"<< r12 << endl;
+    //     cout << "The value of r2 = \n"<< r22 << endl;
+    // }
+    // else
+    // {
+    //     cout << "The roots are real and different\n";
+    // }
+    // cout << "The value of r1 = \n"<< r12 << endl;
+    // cout << "The value of r2 = \n"<< r22 << endl;
+
+    return 0;
+}
