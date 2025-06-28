7bccfac017aad01814a4159afd65029dd23d9597
Rishu kumar singh <rishu272018@gmail.com>
2025-06-28 18:08:01 +0530

    insertion sort

--- DSA/OOPM/sorting/selectionSort.cpp
+++ DSA/OOPM/sorting/selectionSort.cpp
@@ -1,3 +1,34 @@
-# include <iostream>
+// Selection Sort:
+//selection Sort
+#include<iostream>
 using namespace std;
+                  // 2  3  1     n=3
+void selectionSort(int arr[],int n){
+    for(int i=0;i<n-1;i++){//i=0,1
+        int minIndex=i;//0
+        for(int j=i+1;j<n;j++){//j=1
+            if(arr[j]>arr[minIndex])
+                minIndex=j;
+           
+        }
+            int t;
+            t=arr[i];
+            arr[i]=arr[minIndex];
+            arr[minIndex]=t;
+       
+    }
+}
 
+int main(){
+    int arr[]={100,10,90,20,70};
+    cout<<"\nBefore Sorting \n";
+    for(int i=0;i<5;i++){
+        cout<<"\t"<<arr[i];
+    }
+    selectionSort(arr,5);
+    cout<<"\nAfter  Sorting Selection Sort \n";
+    for(int i=0;i<5;i++){
+        cout<<"\t"<<arr[i];
+    }
+    return 0;
+}
