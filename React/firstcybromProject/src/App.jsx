e984c57dff2572951316eb9978e387e51b1977d5
Rishu kumar singh <rishu272018@gmail.com>
2025-07-11 19:06:51 +0530

    2Day React

--- React/firstcybromProject/src/App.jsx
+++ React/firstcybromProject/src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const myfunc=(e)=>{
+    e.target.style.color = "blue";
+    e.target.style.backgroundColor = "yellow";
+  }
+
+  // let h = document.getElementById('h1');
+  // h.innerHTML="change by javascript";
+
+  let arr=[1,2,3,4,5,6,7,8,9,10];
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 id='h1' onClick={myfunc} style={{color:"red",backgroundColor:"cyan",cursor:"pointer"}}>Welcome to the First Cybrom Project</h1>
+      <h2>hii I am Rishu</h2>
+      <h3>Here is a list of numbers:</h3>
+      {
+        arr.map((i)=>(
+          <h3>{i}</h3>
+        ))
+      }
+
     </>
   )
 }
