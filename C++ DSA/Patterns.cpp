80f3d85713778bd3b849a25a7520bd1790731103
Rishu kumar singh <rishu272018@gmail.com>
2025-09-09 23:03:56 +0530

    2 patterns

--- C++ DSA/Patterns.cpp
+++ C++ DSA/Patterns.cpp
@@ -3,18 +3,52 @@
 // 1234
 // 1234
 
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number of row";
+//     cin>>n;
+//     for(int i =1; i<=n; i++){
+//         for(int j=1; j<=n ; j++)
+//         {
+//             cout<<i;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// ===================================================================================
+// ABCD
+// ABCD
+// ABCD
+// ABCD
+
+// 
+
+
+// ==================================================================================
+// 123
+// 456
+// 789
+
+#include <iostream>
 using namespace std;
-int main(){
-    int n;
-    cout<<"Enter the number of row";
-    cin>>n;
-    for(int i =1; i<=n; i++){
-        for(int j=1; j<=n ; j++)
+int main()
+{
+    int n=3;
+    // cout << "Enter the number of row";
+    // cin >> n;
+    int count = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= n; j++)
         {
-            cout<<i;
+            cout << count<<" ";
+            count++;
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
