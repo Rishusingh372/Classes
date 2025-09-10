bc7f90347ee3c89685bc346ab36bf48154b488e0
Rishu kumar singh <rishu272018@gmail.com>
2025-09-10 22:17:04 +0530

    3 patterns

--- C++ DSA/Patterns.cpp
+++ C++ DSA/Patterns.cpp
@@ -33,22 +33,104 @@
 // 456
 // 789
 
-#include <iostream>
-using namespace std;
-int main()
-{
-    int n=3;
-    // cout << "Enter the number of row";
-    // cin >> n;
-    int count = 1;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j <= n; j++)
-        {
-            cout << count<<" ";
-            count++;
-        }
-        cout << endl;
-    }
-    return 0;
-}
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n=3;
+//     // cout << "Enter the number of row";
+//     // cin >> n;
+//     int count = 1;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j <= n; j++)
+//         {
+//             cout << count<<" ";
+//             count++;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+// =========================================================================================
+// *
+// **
+// ***
+// ****
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n=3;
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<=i; j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+// =========================================================================================
+// 1
+// 22
+// 333
+
+    // #include <iostream>
+    // using namespace std;
+    // int main(){
+    //     int n =3;
+    //     for(int i=0; i<n; i++){
+    //         for(int j=0; j<=i; j++){
+    //             cout<<i+1;
+    //         }
+    //         cout<<endl;
+    //     }
+    //     return 0;
+    // }
+
+// =========================================================================================
+
+// 1
+// 12
+// 123
+// 1234
+
+            // #include <iostream>
+            // using namespace std;
+            // int main(){
+            //     int n=4;
+            //     for(int i=0; i<n; i++){
+            //         for(int j=0; j<=i; j++){
+            //             cout<<j+1;
+            //         }
+            //         cout<<endl;
+            //     }
+            //     return 0;
+            // }
+
+
+// =========================================================================================
+// 1
+// 21
+// 321
+// 4321
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     for(int i=0; i<n; i++){
+//         for(int j=i+1; j>0; j--){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+// =========================================================================================
