97d94db8ef5027d1e20913c653604c41752d358e
Rishu kumar singh <rishu272018@gmail.com>
2025-09-10 22:41:30 +0530

    Floyd's Triangle ,Pyramid Pattern

--- C++ DSA/Patterns.cpp
+++ C++ DSA/Patterns.cpp
@@ -134,3 +134,74 @@
 
 
 // =========================================================================================
+
+// Floyd's Triangle
+// 1
+// 23
+// 456
+// 78910
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     int count =1;
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<=i; j++){
+//             cout<<count<<" ";
+//             count++;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// =========================================================================================
+
+// 1111
+//  222
+//   33
+//    4
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<i; j++){
+//             cout<<" ";
+//         }
+//         for(int k=0; k<n-i; k++){
+//             cout<<i+1;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+// =========================================================================================
+
+// Pyramid Pattern
+//    1
+//   121
+//  12321
+// 1234321
+
+#include <iostream>
+using namespace std;
+int main(){
+    int n=4;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i-1; j++){
+            cout<<" ";
+        }
+        for(int k=0; k<i+1; k++){
+            cout<<k+1;
+        }
+        for(int l=i; l>0; l--){
+            cout<<l;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
