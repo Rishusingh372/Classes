eae8bba4bb977226541177673b2cbef2f39d7beb
Rishu kumar singh <rishu272018@gmail.com>
2025-09-10 22:51:41 +0530

    function

---
+++ C++ DSA/FunctionC++.cpp
@@ -0,0 +1,20 @@
+// using function calculate factorial of a number
+
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    int fact=1;
+    for(int i=1; i<=n; i++){
+        fact=fact*i;
+    }
+    return fact;
+}
+int main(){
+    int num;
+    cout<<"Enter a number to find factorial: ";
+    cin>>num;
+    int ans = factorial(num);
+    cout<<"Factorial of "<<num<<" is "<<ans<<endl;
+    return 0;
+}
