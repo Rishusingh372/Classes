a10c1ab370b6247711e07da2a7a5f2a1c898e06b
Rishu kumar singh <rishu272018@gmail.com>
2025-09-16 17:30:42 +0530

    server HTTP

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,20 @@
-console.log("Backend/index.js");
+
+// HTTP :- http server use for handling requests form the client and send the response to the client
+
+// http module built-in module in node.js
+// fistly import the http module
+const http = require('http');
+
+// create a server using the http module
+
+const server = http.createServer((req, res) => {
+  res.write("I am From http server");
+  res.end();
+});
+
+// listen the server on port 3000
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+
