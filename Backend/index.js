1666a1ce91cbec77c98a59866de63cafa5d55606
Rishu kumar singh <rishu272018@gmail.com>
2025-09-17 17:29:25 +0530

    route create

--- Backend/index.js
+++ Backend/index.js
@@ -3,24 +3,43 @@
 
 // http module built-in module in node.js
 // fistly import the http module
-const http = require('http');
+// const http = require('http');
+import http from 'http';
+
+let server = http.createServer((req,res)=>{
+  if(req.url=='/'){
+    res.write("<h1>This is Home Page</h1>");
+    res.end();
+  }
+    
+  else if(req.url=='/about'){
+     res.write("<h1>This is About Page</h1>");
+     res.end();
+  }
+    
+  else if(req.url=='/contact')
+    res.write("<h1>This is Contact Page</h1>");
+     res.end();
+})
 
 // create a server using the http module
 
-const server = http.createServer((req, res) => {
-//   res.write("I am From http server");  //normal data
+// const server = http.createServer((req, res) => {
+// //   res.write("I am From http server");  //normal data
 
 
-// for json data
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  const data = {
-    name: "Rishu",
-    job: "Developer",
-    age: 21
-  }
-  res.write(JSON.stringify(data));
-  res.end();
-});
+// // for json data
+//   res.writeHead(200, {'Content-Type': 'application/json'});
+//   const data = {
+//     name: "Rishu",
+//     job: "Developer",
+//     age: 21
+//   }
+//   res.write(JSON.stringify(data));
+//   res.end();
+// });
+
+
 
 // listen the server on port 3000
 server.listen(3000, () => {
