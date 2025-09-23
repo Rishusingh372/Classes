97db01abdef5992e441addff92026a64d8f22d12
Rishu kumar singh <rishu272018@gmail.com>
2025-09-23 17:59:11 +0530

    mongodb

--- Backend/routes/src/index.js
+++ Backend/routes/src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-
+const model = require('./model');
 const mongoose = require('mongoose');
+
+// middleware to parse JSON request bodies
+app.use(express.json());
+app.use(express.urlencoded());
 
 // database
 mongoose.connect('mongodb://localhost:27017/testdb')
@@ -17,17 +21,28 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// params
-app.get('/user/:name/:contact', (req, res) => {
-   res.send("working")
-   console.log(req.params);
-});
 
-// query
-app.get('/demo', (req, res) => {
-   res.send("working")
-   console.log(req.query);
-});
+// insert data
+app.post('add',(req,res)=>{
+  const {name,age,contact}=req.body;
+  const newUser=new model({
+    name,age,contact
+  })
+  newUser.save()
+  res.send("data added")
+})
+  
+// // params
+// app.get('/user/:name/:contact', (req, res) => {
+//    res.send("working")
+//    console.log(req.params);
+// });
+
+// // query
+// app.get('/demo', (req, res) => {
+//    res.send("working")
+//    console.log(req.query);
+// });
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
