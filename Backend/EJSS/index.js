fe97c0285ea398507874511cf181a5b0acdc7aeb
Rishu kumar singh <rishu272018@gmail.com>
2025-09-25 20:13:27 +0530

    backend setup

--- Backend/EJSS/index.js
+++ Backend/EJSS/index.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const app = express();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs'); 
+
+app.get('/',(req,res)=>{
+    res.render('home');
+});
+app.get('/about',(req,res)=>{
+    res.render('about');
+});
+
+app.listen(3340,()=>{
+    console.log("Server started at port 3340 ");
+});
+
 
