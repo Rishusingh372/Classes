b3a0aec42bebdec59539f32ae4d1f27791d0d625
Rishu kumar singh <rishu272018@gmail.com>
2025-10-14 16:29:13 +0530

    updat index

--- Backend/mvc/controllers/stuController.js
+++ Backend/mvc/controllers/stuController.js
@@ -1,5 +1,6 @@
-const { default: mongoose } = require("mongoose");
-const { assign } = require("nodemailer/lib/shared");
+// const { default: mongoose } = require("mongoose");
+// const { assign } = require("nodemailer/lib/shared");
+const Student = require('../models/stuModel');
 
 const homepage = ("/home", (req, res) => {
    res.render('home')
@@ -18,39 +19,43 @@ const contactpage = ("/contact", (req, res) => {
    res.render('contact')
 });
 
-const saveStudent = async(req, res) => {
+// const saveStudent = async(req, res) => {
+//    console.log(req.body);
+//    const Student = require('../models/stuModel');
+//    const newStudent = new Student({
+//       rollNo: req.body.rn,
+//       name: req.body.nm,
+//       city: req.body.ct,
+//       fee: req.body.fs
+//    });
+//    try {
+//       await newStudent.save();
+//       res.redirect('about');
+//    }
+//    catch (err) {
+//       console.log(err);
+//       res.status(500).send('Error saving student data');
+//    }
+// }
+
+const saveStudent = async (req, res) => {
+   const{ rn, nm, ct, fs } = req.body;
    console.log(req.body);
-   const Student = require('../models/stuModel');
-   const newStudent = new Student({
-      rollNo: req.body.rn,
-      name: req.body.nm,
-      city: req.body.ct,
-      fee: req.body.fs
-   });
+
    try {
-      await newStudent.save();
+      const student = await Student.create({
+         rollNo: rn,
+         name: nm,
+         city: ct,
+         fee: fs
+      });
       res.redirect('about');
-   }
-   catch (err) {
+   } catch (err) {
       console.log(err);
       res.status(500).send('Error saving student data');
    }
 }
 
-// const saveStudent = async (req, res) => {
-//    const{ rn, nm, ct, fs } = req.body;
-//    console.log(req.body);
-
-//    const student = await mongoose.create({
-//       rollNo: rn,
-//       name: nm,
-//       city: ct,
-//       fee: fs
-//    });
-//    // student.save();
-//    res.redirect('about');
-
-// }
   
 
 module.exports = {
