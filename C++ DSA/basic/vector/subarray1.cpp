9fa1d8c5332507c3d5b8ffe06b7aaf598dfb381d
Rishu kumar singh <rishu272018@gmail.com>
2025-10-19 22:39:43 +0530

    Minimum Subarray Sum

--- C++ DSA/basic/vector/subarray1.cpp
+++ C++ DSA/basic/vector/subarray1.cpp
@@ -67,23 +67,45 @@
 // }
 
 // find maximum subarray sum using kadane's algorithm (handling all negative numbers)
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main(){
+//     vector<int> arr = {-2,-3,-1,-4};
+//     int n = arr.size();
+//     int maxSum = INT32_MIN;
+//     int currSum = 0;
+//     for(int i=0; i<n; i++){
+//         currSum += arr[i];
+//         if(currSum > maxSum){
+//             maxSum = currSum;
+//         }
+//         if(currSum < 0){
+//             currSum = 0;
+//         }
+//     }
+//     cout << "Maximum Subarray Sum is: " << maxSum << endl;
+//     return 0;
+// }
+
+// find lowest subarray sum using kadane's algorithm
 #include <iostream>
 #include <vector>
 using namespace std;
 int main(){
-    vector<int> arr = {-2,-3,-1,-4};
+    vector<int> arr = {2,3,-4,1,-2,6,-1};
     int n = arr.size();
-    int maxSum = INT32_MIN;
+    int minSum = INT32_MAX;
     int currSum = 0;
     for(int i=0; i<n; i++){
         currSum += arr[i];
-        if(currSum > maxSum){
-            maxSum = currSum;
+        if(currSum < minSum){
+            minSum = currSum;
         }
-        if(currSum < 0){
+        if(currSum > 0){
             currSum = 0;
         }
     }
-    cout << "Maximum Subarray Sum is: " << maxSum << endl;
+    cout << "Minimum Subarray Sum is: " << minSum << endl;
     return 0;
 }
