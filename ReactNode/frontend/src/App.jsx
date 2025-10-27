9304249eccaf1391f5595067e30a18651370b9f9
Rishu kumar singh <rishu272018@gmail.com>
2025-10-27 19:31:46 +0530

    initial setup of backend with react

---
+++ ReactNode/frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const App =()=>{
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/home');
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  const postData = async()=>{
+    try{
+      const response = await axios.post('http://localhost:5000/data',{name:"Rishabh",age:21});
+      console.log(response.data);
+    }
+    catch(error){
+      console.error('Error posting data:', error);
+    }
+  }
+
+  return(
+    <>
+     <h1>Welcome to my App</h1>
+      <button onClick={fetchData}>Fetch Data from Backend</button>
+
+      <button onClick={postData}> Post Data</button>
+    </>
+  )
+}
+
+export default App;
