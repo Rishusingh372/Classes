e71d53ea899f7dc07ee72938df98c0fce073b274
Rishu kumar singh <rishu272018@gmail.com>
2025-10-28 07:22:40 +0530

    add form and update all code and add css

--- ReactNode/frontend/src/pages/Contact.jsx
+++ ReactNode/frontend/src/pages/Contact.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+   <>
+    <h1>Collect Data Form</h1>
+    <form>
+       Name <input type="text" id="fname" name="fname" /><br />
+       Email<input type="email" id="email" name="email" /><br />
+       Number<input type="number" id="number" name="number" /><br />
+        <input type="submit" value="Submit" />
+    </form>
+   </>
   )
 }
 
