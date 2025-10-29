4095174f0fe9564fefe33a54deeb82b97e0c2a2b
Rishu kumar singh <rishu272018@gmail.com>
2025-10-29 09:20:23 +0530

    add forms and basic setup for curd opertion

--- ReactNode/frontend/src/App.jsx
+++ ReactNode/frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Nab from "./components/Nab";
+import Insert from "./pages/Insert";
+import Nab from "./components/Nav";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Service from "./pages/Service";
-import Contact from "./pages/Contact";
+import Display from "./pages/Display";
+// import Contact from "./pages/Contact";
+import Update from "./pages/Update";
+import Serach from "./pages/Serach";
 import Conection from "./pages/Conection";
 import Footer from "./components/Footer";
 
@@ -14,12 +16,13 @@ function App (){
       <Nab />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/Insert" element={<Insert />} />
+          <Route path="/Display" element={<Display />} />
+          <Route path="/update" element={<Update />} />
+          <Route path="/search" element={<Serach />} />
           <Route path="/conection" element={<Conection />} />
         </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </Router>
   )
 
