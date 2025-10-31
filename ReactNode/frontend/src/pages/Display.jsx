a3145b2e2e715c409d9bf2df5251afb925d65b0b
Rishu kumar singh <rishu272018@gmail.com>
2025-10-31 07:28:15 +0530

    DisplayData

--- ReactNode/frontend/src/pages/Display.jsx
+++ ReactNode/frontend/src/pages/Display.jsx
@@ -1,8 +1,72 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react';
 
+import axios from "axios"
+import { Link } from 'react-router-dom';
 const Display = () => {
+
+  const [stuData, setempData] = useState([]);
+  const [loading , setloading] = useState(true)
+
+
+  useEffect(() => {
+      const api = "http://localhost:5000/empoyees/empinfo"
+      const response =  axios.get(api);
+      response.then((res)=>{
+        setempData(res.data);
+        setloading(false)
+      }).catch((err)=>{
+        console.log("data fetch susscesfull")
+      })
+      
+  },[])
+
+   const deletedata = async (id) =>{
+    const api = `http://localhost:5000/empoyees/delete/${id}`;
+    const response = await axios.delete(api);
+    alert("data deleted successfully")
+     setempData(response.data);
+   }
   return (
-    <div>Display</div>
+    <>
+      <h1>This is Display Component</h1>
+      <h2>Student Info Page</h2>
+     
+     {
+           loading ? (
+            <p>page loading ...</p>
+           ) :( 
+            <table bgcolor="pink" align="center" width="600" border="1px solid black">
+              <thead>
+                <tr>
+                  <th>id</th>
+                  <th>name</th>
+                  <th>location</th>
+                  <th>subject</th>
+                  <th>edit / delete</th>
+                  
+                </tr>
+              </thead>
+              <tbody  align="center" width="600"  >
+                {
+                  stuData.map((e, index)=>(
+                    <tr key={index}>
+                      <td>{e.id}</td>
+                      <td>{e.name}</td>
+                      <td>{e.location}</td>
+                      <td>{e.inrollsub}</td>
+                      <td> <Link to={`/update/${e._id}`}>Edit</Link> <Link><button onClick={()=>deletedata(e._id)}>delete</button></Link></td>
+                    </tr>
+                  )
+                  )
+                }
+              </tbody>
+            </table>
+           )
+     }
+
+    </>
   )
 }
 
