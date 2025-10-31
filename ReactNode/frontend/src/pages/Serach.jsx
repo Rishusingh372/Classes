a2b0a9faecd184c0b39ebed2d7de2f2dec6e47b2
Rishu kumar singh <rishu272018@gmail.com>
2025-10-31 08:02:57 +0530

    complete curd opertion

--- ReactNode/frontend/src/pages/Serach.jsx
+++ ReactNode/frontend/src/pages/Serach.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { useState , useEffect } from 'react'
+import axios from 'axios'
+const Search = () => {
+  const [query , setQuery] = useState("")
+  const [searchdata , setSearchdata] = useState([]);
+  const handlchange = (e)=>{
+    setQuery(e.target.value);
+  }
+ 
 
-const Serach = () => {
+
+   const searchData = async()=>{
+     const api = "http://localhost:5000/employees/searchdata";
+      const response = await axios.post(api , {id:query});
+      console.log(response.data)
+      setSearchdata(response.data);
+   }
+   
+
   return (
-    <div>Serach</div>
+    <>
+        <h1>This is Search Component</h1>
+        search data : <input type="text"  value={query} onChange={handlchange}/>
+        <button onClick={searchData}>search</button>
+        <h2>Search Result :</h2>
+        {
+          searchdata.map((e)=>(
+            <div key={e._id}>
+              <h1>id : {e.id}</h1>
+              <h1>name : {e.name}</h1>
+              <h1>location : {e.location}</h1>
+              <h1>subject : {e.inrollsub}</h1>
+            </div>
+          ))
+        }
+        {/* <h1>id : {searchdata.id}</h1>
+        <h1>name : {searchdata.name}</h1>
+        <h1>location : {searchdata.location}</h1>
+        <h1>subject : {searchdata.inrollsub}</h1>
+        <h1>rollNo : {searchdata.sturoll}</h1> */}
+    </>
   )
 }
 
-export default Serach
+export default Search
