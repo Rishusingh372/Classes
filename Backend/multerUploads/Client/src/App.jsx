5f5a16df2e72daf85f04bec8c62a366ea9ba82e4
Rishu kumar singh <rishu272018@gmail.com>
2025-11-22 16:59:04 +0530

    multer with axios

--- Backend/multerUploads/Client/src/App.jsx
+++ Backend/multerUploads/Client/src/App.jsx
@@ -1,22 +1,36 @@
 import { useState } from "react";
+import axios from "axios";
 
 function App() {
   const [file, sefile] = useState(0);
   const handleImage = (e) => {
     sefile(e.target.files[0]);
   };
+    // const handleSubmit = async () => {
+    //   const formdata = new FormData();
+    //   formdata.append("file", file);
+    //   try {
+    //     const res = await fetch("http://localhost:8000/upload", {
+    //       method: "POST",
+    //       body: formdata,
+    //     });
+    //     const data = await res.text();
+    //     console.log(data);
+    //   } catch (error) {
+    //     console.log("Error while uploading file:", error);
+    //   }
+    // };
+    
   const handleSubmit = async () => {
     const formdata = new FormData();
+    let api ="http://localhost:8000/upload"
     formdata.append("file", file);
     try {
-      const res = await fetch("http://localhost:8000/upload", {
-        method: "POST",
-        body: formdata,
-      });
-      const data = await res.text();
-      console.log(data);
+      const res = await axios.post(api, formdata);
+      console.log(res.data);
     } catch (error) {
       console.log("Error while uploading file:", error);
+      
     }
   };
 
