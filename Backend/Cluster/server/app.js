5932e01ad69394e0eac7167e53e1ea44fb1fd75e
Rishu kumar singh <rishu272018@gmail.com>
2025-12-03 19:24:02 +0530

    Cluster

--- Backend/Cluster/server/app.js
+++ Backend/Cluster/server/app.js
@@ -1,75 +1,31 @@
-// const buffer1 = Buffer.alloc(100);
-// const buffer2 = new Buffer('cybrom');
-// const buffer3=Buffer.from([1,2,3,4]);
 
-// console.log(buffer1);
-// console.log(buffer2);
-// console.log(buffer3);
+// const os = require('os');
+// console.log(os.cpus().length); // Number of CPU cores
 
+const cluster = require("cluster");
+const http = require("http");
+const os = require("os");
+if (cluster.isMaster) {
+    // Master process
+    console.log(`Master process ${process.pid} is running`);
+    // Get number of CPU cores
+    const numCPUs = os.cpus().length;
+    // Fork workers
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    // Listen for worker exit
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`);
+        console.log("Starting a new worker...");
+        cluster.fork(); // Restart a new worker if one dies
+    });
+} else {
+    // Worker processes
+    http.createServer((req, res) => {
+        res.writeHead(200);
+        res.end(`Hello from Worker ${process.pid}\n`);
+    }).listen(3000);
+    console.log(`Worker ${process.pid} started`);
+}
 
-// // Writing data to Buffer
-// buffer1.write('happy')
-// console.log(buffer1)
-
-// // Reading data from Buffer
-
-// const a=buffer1.toString('utf-8')
-// console.log(a);
-
-// // check object is Buffer or not
-
-// console.log(Buffer.isBuffer(buffer1));
-
-// // check the length of Buffer
-// console.log(buffer1.length)
-
-// // copy buffer
-// const bufferSrc=new Buffer('ABC')
-// const bufferDest= Buffer.alloc(3);
-// bufferSrc.copy(bufferDest);
-// console.log(bufferDest);
-
-// const Data = bufferDest.toString('utf-8')
-// console.log(Data);
-
-// // Slicing Data
-// const bufferOld = new Buffer('welcom To cybrom');
-// const bufferNew = bufferOld.slice(0,4);
-// console.log(bufferNew.toString());
-
-// // Concate Buffer
-// const bufferone= new Buffer('cybrom');
-// const buffertwo= new Buffer(' is a company');
-// const bufferthree= Buffer.concat([bufferone, buffertwo]);
-// console.log(bufferthree.toString());
-
-// // Comparing Buffers
-// const bufferA = new Buffer('cybrom');
-// const bufferB = new Buffer('cybrom');
-// const bufferC = new Buffer('cybrom1');
-// console.log(bufferA.equals(bufferB)); // true
-// console.log(bufferA.equals(bufferC)); // false  
-
-// console.log(bufferA.compare(bufferB)); // 0
-// console.log(bufferA.compare(bufferC)); // -1 (bufferA is less than bufferC)
-// console.log(bufferC.compare(bufferA)); // 1 (bufferC is greater than bufferA)   
-// console.log(bufferA.compare(bufferA)); // 0 (same buffer)
-
-// // Fill Buffer
-// const bufferFill = Buffer.alloc(10);
-// bufferFill.fill('x');
-// console.log(bufferFill.toString());
-
-
-// // Buffer to JSON
-// const bufferJson = Buffer.from('Hello, World!');
-// const json = bufferJson.toJSON();
-// console.log(json);
-
-// // JSON to Buffer
-// const jsonBuffer = Buffer.from(json.data);
-// console.log(jsonBuffer.toString());
-
-const os = require('os');
-
-console.log(os.cpus().length); // Number of CPU cores
