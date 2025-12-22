360a18024feeef92be584a42e2e64e9b37868527
Rishu kumar singh <rishu272018@gmail.com>
2025-12-22 22:56:24 +0530

    Update all code and add feture of Edit , update

--- Backend/To_Do Using Redux/client/src/App.jsx
+++ Backend/To_Do Using Redux/client/src/App.jsx
@@ -1,74 +1,95 @@
 import { useState } from "react";
-import { addTask, delTask, setStatus } from "./todoSlice"; // Import setStatus
+import { addTask, delTask, taskComplete, taskIncomplete, taskUpdate } from "./todoSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { MdDeleteSweep } from "react-icons/md";
 import { FaCircleCheck } from "react-icons/fa6";
 import { ImCross } from "react-icons/im";
-
 const App=()=>{
   const [val, setVal]= useState("");
+  const [btnStatus, setBtnstatus]= useState(true);
+  const [taskId, setTaskId] = useState("");
   const dispatch = useDispatch();
   const myTask= useSelector(state=>state.todo.task);
+  console.log(myTask);
 
-  const ans = myTask.map((key, index) => {
 
-  let myStyle = {};
-
-  if (key.status === true) {
-    myStyle = {
-      textDecoration: "line-through",
-      color: "red"
-    };
-  } else {
-    myStyle = {
-      textDecoration: "none",
-      color: "black"
-    };
+  const taskEdit=(tsk, id)=>{
+    setVal(tsk)
+    setBtnstatus(false);
+    setTaskId(id);
   }
 
-  return (
-    <tr key={key.id}>
-      <td>{index + 1}</td>
-      <td style={myStyle}>
-        {key.work}
-      </td>
+  const myUpdate=()=>{
+    dispatch(taskUpdate({id:taskId, work:val}));
+     setBtnstatus(true);
+     setVal("");
+  }
 
-      <td>
-        <MdDeleteSweep onClick={() => { dispatch(delTask({ id: key.id })) }} />
-      </td>
-      <td>
-        <FaCircleCheck onClick={() => { dispatch(setStatus({ id: key.id, status: true })) }} />
-      </td>
-      <td>
-        <ImCross onClick={() => { dispatch(setStatus({ id: key.id, status: false })) }} />
-      </td>
-      <td> edit </td>
-    </tr>
-  );
+
+
+  const ans= myTask.map((key, index)=>{
+     return(
+      <>
+         <tr>
+          <td>{index+1}</td>
+          <td>
+            
+            {key.status=="complete"?(<>
+              <span style={{color:"red",textDecoration: "line-through" }}>{key.work}</span>
+            </>):(<>
+                    {key.work}
+            </>)}
+          
+            
+            
+            </td>
+          <td>
+            <MdDeleteSweep onClick={()=>{dispatch(delTask({id:key.id}))}} />
+          </td>
+          <td>
+            <FaCircleCheck onClick={()=>{dispatch(taskComplete({id:key.id}))}} />
+          </td>
+          <td onClick={()=>{dispatch(taskIncomplete({id:key.id}))}}>
+            <ImCross />
+          </td>
+          <td>
+            <button onClick={()=>{taskEdit(key.work, key.id)}}>Edit</button>
+          </td>
+         </tr>
+      </>
+     )
   })
-
   return(
     <>
       <h1> To Do App </h1>
       <hr />
       Enter Task : <input type="text" value={val} onChange={(e)=>{setVal(e.target.value)}} />
-      <button onClick={()=>{dispatch(addTask({id: Date.now(), work:val}))}}>Add</button>
+    {btnStatus?(<>
+    
+     <button onClick={()=>{dispatch(addTask({id: Date.now(), work:val, status:"incomplete"}))}}>Add</button>
+    </>):(<>
+     
+      <button onClick={myUpdate}>update</button>
+    </>)}
+    
+     
+    
+     
+    
+    
       <hr />
-      <table border="1" cellPadding="10">
-        <thead>
-          <tr>
-            <th>SNo</th>
-            <th>Assigned Task</th>
-            <th>Delete</th>
-            <th>Check</th>
-            <th>Crose</th>
-            <th>Edit</th>
-          </tr>
-        </thead>
-        <tbody>
-          {ans}
-        </tbody>
+      <table>
+        <tr>
+          <th>SNo</th>
+          <th> You Assigned Task</th>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
+        {ans}
       </table>
+
     </>
   )
 }
--- Backend/To_Do Using Redux/client/src/todoSlice.jsx
+++ Backend/To_Do Using Redux/client/src/todoSlice.jsx
@@ -1,28 +1,48 @@
 import { createSlice } from "@reduxjs/toolkit";
-
 const todoSlice = createSlice({
      name:"todo",
      initialState:{
         task:[]
      },
      reducers:{
-        addTask:(state, actions)=>{  
-            // Add status property to new tasks
-            state.task.push({...actions.payload, status: false});
+        addTask:(state, actions)=>{
+           
+            state.task.push(actions.payload);
         },
+
         delTask:(state, actions)=>{
-            state.task= state.task.filter(item=>item.id!=actions.payload.id);      
+            state.task= state.task.filter(item=>item.id!=actions.payload.id)
+           
         },
-        // New reducer to change status
-        setStatus:(state, actions)=>{
-            for (var i=0; i<state.task.length; i++) {
-                if (state.task[i].id === actions.payload.id) {
-                    state.task[i].status = actions.payload.status;
+        taskComplete:(state, actions)=>{
+            for(var i=0; i<state.task.length; i++){
+                 if (state.task[i].id==actions.payload.id)
+                 {
+                    state.task[i].status="complete";
+                 }
+            }
+        },
+        taskIncomplete:(state, actions)=>{
+             for(var i=0; i<state.task.length; i++){
+                 if (state.task[i].id==actions.payload.id)
+                 {
+                    state.task[i].status="incomplete";
+                 }
+            }
+        },
+
+        taskUpdate:(state, actions)=>{
+            for (var i=0; i<state.task.length; i++)
+            {
+                if (state.task[i].id==actions.payload.id)
+                {
+                    state.task[i].work=actions.payload.work;
                 }
             }
+           
         }
      }
 })
 
-export const {addTask, delTask, setStatus}  = todoSlice.actions;
+export const {addTask, delTask, taskComplete, taskIncomplete, taskUpdate}  = todoSlice.actions;
 export default todoSlice.reducer;
